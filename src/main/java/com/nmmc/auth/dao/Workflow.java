61f983960bdf04fa9b0fb06c0e18aabd5d9a5ca4
aakashchavhan007 <aakashchavhan007@gmail.com>
2025-07-31 14:32:38 +0530

    Update Workflow.java

--- src/main/java/com/nmmc/auth/dao/Workflow.java
+++ src/main/java/com/nmmc/auth/dao/Workflow.java
@@ -1,1 +1,18 @@
+package com.nmmc.auth.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Workflow {
+
+    private String templateName;
+    
+    private List<WorkflowDetails> workflowDetails;
+}
 
