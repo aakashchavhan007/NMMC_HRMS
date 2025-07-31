3f10e574bc208a75edd94d0c62eb3e98c4da8219
aakashchavhan007 <aakashchavhan007@gmail.com>
2025-07-31 14:52:31 +0530

    Create DuplicateRecordException.java

---
+++ src/main/java/com/nmmc/hrms/Exceptions/DuplicateRecordException.java
@@ -0,0 +1,9 @@
+package com.nmmc.hrms.Exceptions;
+
+public class DuplicateRecordException extends RuntimeException {
+	
+	 public DuplicateRecordException(String message) {
+	        super(message);
+	    }
+
+}
