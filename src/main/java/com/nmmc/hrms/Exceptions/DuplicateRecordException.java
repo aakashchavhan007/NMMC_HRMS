1c3341fc606573310df2f63c9fb025109d4aa3ac
aakashchavhan007 <aakashchavhan007@gmail.com>
2025-07-31 15:54:01 +0530

    Delete src/main/java/com/nmmc/hrms/Exceptions directory

--- src/main/java/com/nmmc/hrms/Exceptions/DuplicateRecordException.java
+++
@@ -1,9 +0,0 @@
-package com.nmmc.hrms.Exceptions;
-
-public class DuplicateRecordException extends RuntimeException {
-	
-	 public DuplicateRecordException(String message) {
-	        super(message);
-	    }
-
-}
