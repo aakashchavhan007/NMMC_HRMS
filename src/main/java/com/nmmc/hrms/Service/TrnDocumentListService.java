f80eac0ba633d9a73e92ea4570740c1b31ee3a35
aakashchavhan007 <aakashchavhan007@gmail.com>
2025-07-31 14:49:45 +0530

    Update TrnDocumentListService.java

--- src/main/java/com/nmmc/hrms/Service/TrnDocumentListService.java
+++ src/main/java/com/nmmc/hrms/Service/TrnDocumentListService.java
@@ -1,1 +1,16 @@
+package com.nmmc.hrms.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.nmmc.hrms.Dao.TrnDocumentListDao;
+
+public interface TrnDocumentListService {
+
+    String save(TrnDocumentListDao trnDocumentListDao);
+
+    List<TrnDocumentListDao> findAllDocumentListByTransactionIdAndTransactionKey(UUID transactionId,
+            String transactionKey);
+
+}
 
