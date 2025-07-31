b411367b1c03f78979c89c1a1abda54b4b0ba4b2
aakashchavhan007 <aakashchavhan007@gmail.com>
2025-07-31 14:32:55 +0530

    Update WorkflowDetails.java

--- src/main/java/com/nmmc/auth/dao/WorkflowDetails.java
+++ src/main/java/com/nmmc/auth/dao/WorkflowDetails.java
@@ -1,1 +1,15 @@
+package com.nmmc.auth.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkflowDetails {
+
+    private String stage;
+    
+}
 
